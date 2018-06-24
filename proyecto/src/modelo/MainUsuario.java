c771e1f38802fe5301aac517d23ebbebaf60f429
Sidihidi <40538007+Sidihidi@users.noreply.github.com>
2018-06-24 14:10:09 +0200

    Add files via upload

---
+++ proyecto/src/modelo/MainUsuario.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+import vista.Pantalla_inicio;
+/**
+ *
+ * @author Juan
+ */
+public class MainUsuario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Pantalla_inicio log = new Pantalla_inicio();
+        log.show();
+    }
+    
+}
---
+++ proyecto/src/modelo/Usuario.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import modelo.BD;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Juan
+ */
+public class Usuario {
+    Connection cn;
+    public String nom_usu,password,cargo;
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    
+    public Usuario(){
+        BD con = new BD();
+        cn= con.conexion();
+    }
+
+    public String getNom_usu() {
+        return nom_usu;
+    }
+
+    public void setNom_usu(String nom_usu) {
+        this.nom_usu = nom_usu;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public void ingreso(){
+        try{
+            String id;
+            
+         PreparedStatement cmd = cn.prepareStatement("select Nom_usu,Password,cargo from usuario where Nom_usu=?");
+         cmd.setString(1,getNom_usu());
+         ResultSet rs = cmd.executeQuery() ;
+         
+         if(rs.next()){
+             id = rs.getString(1);
+             this.setPassword(rs.getString(2));
+             this.setCargo(rs.getString(3));
+         }
+        }
+        catch(Exception ex){
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+        
+    }
+}
